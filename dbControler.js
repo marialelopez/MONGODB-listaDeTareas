3d4f05c2a510a58d7203c00ae4ef1e4767db82f0
Alejandra Lopez <mlopezalejandra13@gmail.com>
2023-10-13 20:26:48 -0500

    funciones y metodos http con database para listaDeTareas

---
+++ dbControler.js
@@ -0,0 +1,194 @@
+const { json } = require("express");
+const { MongoClient } = require("mongodb");
+const env = require("dotenv").config();
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+// Función para conectar a la base de datos
+async function conectarBaseDatos() {
+  try {
+    await client.connect();
+    const database = client.db("listaDeTareas");
+    const tareasCollection = database.collection("tareas");
+    const usuarioCollection = database.collection("usuario");
+    return {
+      tareas: tareasCollection,
+      usuario: usuarioCollection,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Función para leer documentos de la colección "tareas"
+async function leerDocumentosTareas() {
+  const collections = await conectarBaseDatos();
+  const tareasCollection = collections.tareas;
+
+  try {
+    const tareas = await tareasCollection.find({}).toArray();
+
+    return {
+      tareas: tareas,
+    };
+  } catch (error) {
+    console.error("Error al obtener los documentos:", error);
+    return null;
+  }
+}
+
+leerDocumentosTareas().then((result) => {
+  if (result) {
+    console.log("Tareas:", result.tareas);
+  }
+});
+
+leerDocumentosTareas();
+
+// Función para leer documentos de la colección "usuarios"
+async function leerDocumentosUsuario() {
+  const collections = await conectarBaseDatos();
+  const usuarioCollection = collections.usuario;
+
+  try {
+    const usuarios = await usuarioCollection.find({}).toArray(); // Obtenemos los usuarios
+
+    return {
+      usuarios: usuarios,
+    };
+  } catch (error) {
+    console.error("Error al obtener los documentos:", error);
+    return null;
+  }
+}
+
+leerDocumentosUsuario().then((result) => {
+  if (result) {
+    console.log("Usuarios:", result.usuarios);
+  }
+});
+
+leerDocumentosUsuario();
+
+// Función para crear un documento en la colección "tareas"
+async function crearTarea(descripcion, completada, fechaVencimiento) {
+  const collections = await conectarBaseDatos();
+  const tareasCollection = collections.tareas;
+
+  try {
+    const nuevaTarea = { descripcion, completada, fechaVencimiento };
+    const resultado = await tareasCollection.insertOne(nuevaTarea);
+    return resultado.insertedId;
+  } catch (error) {
+    console.error("Error al crear la tarea:", error);
+    return null;
+  }
+}
+
+// Función para crear un documento en la colección "usuarios"
+async function crearUsuario(email, password, rol) {
+  const collections = await conectarBaseDatos();
+  const usuarioCollection = collections.usuario;
+
+  try {
+    const nuevoUsuario = { email, password, rol };
+    const resultado = await usuarioCollection.insertOne(nuevoUsuario);
+    return resultado.insertedId;
+  } catch (error) {
+    console.error("Error al crear el usuario:", error);
+    return null;
+  }
+}
+
+// Función para eliminar un documento en la colección "tareas" por descripción
+async function eliminarTareaPorDescripcion(descripcion) {
+  const collections = await conectarBaseDatos();
+  const tareasCollection = collections.tareas;
+
+  try {
+    const resultado = await tareasCollection.deleteOne({
+      descripcion: descripcion,
+    });
+    if (resultado.deletedCount === 1) {
+      return true; // Éxito al eliminar
+    }
+    return false; // No se encontró el documento con la descripción especificada
+  } catch (error) {
+    console.error("Error al eliminar la tarea:", error);
+    return false; // Error al eliminar
+  }
+}
+
+// Función para eliminar un documento en la colección "usuarios" por email
+async function eliminarUsuarioPorEmail(email) {
+  const collections = await conectarBaseDatos();
+  const usuarioCollection = collections.usuario;
+
+  try {
+    const resultado = await usuarioCollection.deleteOne({ email: email });
+    if (resultado.deletedCount === 1) {
+      return true; // Éxito al eliminar
+    }
+    return false; // No se encontró el documento con el email especificado
+  } catch (error) {
+    console.error("Error al eliminar el usuario:", error);
+    return false; // Error al eliminar
+  }
+}
+
+// Función para editar una tarea por descripción
+async function editarTareaPorDescripcion(descripcion, nuevaDescripcion, nuevaCompletada, nuevaFechaVencimiento) {
+    const collections = await conectarBaseDatos();
+    const tareasCollection = collections.tareas;
+  
+    try {
+      const resultado = await tareasCollection.updateOne(
+        { descripcion: descripcion },
+        { $set: { descripcion: nuevaDescripcion, completada: nuevaCompletada, fechaVencimiento: nuevaFechaVencimiento } }
+      );
+  
+      if (resultado.modifiedCount === 1) {
+        return true; // Éxito al editar
+      }
+      return false; // No se encontró el documento con la descripción especificada
+    } catch (error) {
+      console.error("Error al editar la tarea:", error);
+      return false; // Error al editar
+    }
+  }
+  
+//   // Función para editar un usuario por email
+//   async function editarUsuarioPorEmail(email, nuevoEmail, nuevoPassword, nuevoRol) {
+//     const collections = await conectarBaseDatos();
+//     const usuarioCollection = collections.usuario;
+  
+//     try {
+//       const resultado = await usuarioCollection.updateOne(
+//         { email: email },
+//         { $set: { email: nuevoEmail, password: nuevoPassword, rol: nuevoRol } }
+//       );
+  
+//       if (resultado.modifiedCount === 1) {
+//         return true; // Éxito al editar
+//       }
+//       return false; // No se encontró el documento con el email especificado
+//     } catch (error) {
+//       console.error("Error al editar el usuario:", error);
+//       return false; // Error al editar
+//     }
+//   }
+  
+
+module.exports = {
+  conectarBaseDatos,
+  leerDocumentosTareas,
+  leerDocumentosUsuario,
+  crearTarea,
+  crearUsuario,
+  eliminarTareaPorDescripcion,
+  eliminarUsuarioPorEmail,
+  editarTareaPorDescripcion,
+//   editarUsuarioPorEmail
+};
---
+++ server.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const funcionesCRUD = require("./dbControler");
+
+app.use(express.json());
+
+//ruta para leer documentos de la coleccion "tareas"
+app.get("/tareas", async (req, res) => {
+  try {
+    const tareas = await funcionesCRUD.leerDocumentosTareas();
+    res.status(200).json(tareas);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener las tareas" });
+  }
+});
+
+//ruta para leer documentos de la coleccion "usuario"
+app.get("/usuarios", async (req, res) => {
+  try {
+    const usuarios = await funcionesCRUD.leerDocumentosUsuario();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener los usuarios" });
+  }
+});
+
+// Ruta para crear una nueva tarea
+app.post("/crearTarea", async (req, res) => {
+  const { descripcion, completada, fechaVencimiento } = req.body;
+  const tareaId = await funcionesCRUD.crearTarea(
+    descripcion,
+    completada,
+    fechaVencimiento
+  );
+  if (tareaId) {
+    res.status(201).json({ message: "Tarea creada", taskId: tareaId });
+  } else {
+    res.status(500).json({ message: "Error al crear la tarea" });
+  }
+});
+
+// Ruta para crear un nuevo usuario
+app.post("/crearUsuarios", async (req, res) => {
+  const { email, password, rol } = req.body;
+  const usuarioId = await funcionesCRUD.crearUsuario(email, password, rol);
+  if (usuarioId) {
+    res.status(201).json({ message: "Usuario creado", userId: usuarioId });
+  } else {
+    res.status(500).json({ message: "Error al crear el usuario" });
+  }
+});
+
+//   // Ruta para eliminar una tarea por descripcion
+
+app.delete("/tareas/:descripcion", async (req, res) => {
+  const { descripcion } = req.params;
+  const eliminado = await funcionesCRUD.eliminarTareaPorDescripcion(
+    descripcion
+  );
+
+  if (eliminado) {
+    res.status(200).json({ message: "Tarea eliminada con éxito" });
+  } else {
+    res
+      .status(404)
+      .json({
+        message: "No se encontró la tarea o hubo un error al eliminarla",
+      });
+  }
+});
+
+//   // Ruta para eliminar un usuario por email
+
+app.delete("/usuarios/:email", async (req, res) => {
+  const { email } = req.params;
+  const eliminado = await funcionesCRUD.eliminarUsuarioPorEmail(email);
+
+  if (eliminado) {
+    res.status(200).json({ message: "Usuario eliminado con éxito" });
+  } else {
+    res
+      .status(404)
+      .json({
+        message: "No se encontró el usuario o hubo un error al eliminarlo",
+      });
+  }
+});
+
+// Ruta para editar una tarea por descripción
+app.put('/tareas/:descripcion', async (req, res) => {
+    const { descripcion } = req.params;
+    const { nuevaDescripcion, nuevaCompletada, nuevaFechaVencimiento } = req.body;
+    const editado = await funcionesCRUD.editarTareaPorDescripcion(
+      descripcion, nuevaDescripcion, nuevaCompletada, nuevaFechaVencimiento
+    );
+  
+    if (editado) {
+      res.status(200).json({ message: 'Tarea editada con éxito' });
+    } else {
+      res.status(404).json({ message: 'No se encontró la tarea o hubo un error al editarla' });
+    }
+  });
+  
+//   // Ruta para editar un usuario por email
+//   app.put('/usuarios/:email', async (req, res) => {
+//     const { email } = req.params;
+//     const { nuevoEmail, nuevoPassword, nuevoRol } = req.body;
+//     const editado = await funcionesCRUD.editarUsuarioPorEmail(
+//       email, nuevoEmail, nuevoPassword, nuevoRol
+//     );
+  
+//     if (editado) {
+//       res.status(200).json({ message: 'Usuario editado con éxito' });
+//     } else {
+//       res.status(404).json({ message: 'No se encontró el usuario o hubo un error al editarlo' });
+//     }
+//   });
+  
+app.listen(port, () => {
+  console.log("servidor corriendo");
+});
